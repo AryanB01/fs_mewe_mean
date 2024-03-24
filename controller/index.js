20c7530476028dbd48ae4cbd8d706d485046dffb
AryanB01 <aryanbhattarai000@gmail.com>
2024-03-23 21:44:40 -0400

    nav and layout added

--- controller/index.js
+++ controller/index.js
@@ -5,5 +5,10 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.get('/about', function(req, res, next) {
+  res.render('chat', { title: 'About Page' });
+});
+router.get('/chat', function(req, res, next) {
+  res.render('chat', { title: 'Chat Page' });
+});
 module.exports = router;
